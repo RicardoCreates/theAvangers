3e2c78ae41a50e52b0d8f0be4e7421b473ceb54c
Mitterand <asmitterand@yahoo.fr>
2024-08-16 16:27:32 +0200

    ich bin wieder da

--- src/main/java/de/ricardo/Main.java
+++ src/main/java/de/ricardo/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Avangers");
         System.out.println("Hello Avangers");
+        System.out.println("I am a Avangers");
     }
 }
