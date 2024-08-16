c2a675a0562c0a7c617b0d2276e4cb6515b04d17
Mitterand <asmitterand@yahoo.fr>
2024-08-16 16:32:39 +0200

    endlich geschaft

--- src/main/java/de/ricardo/Main.java
+++ src/main/java/de/ricardo/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Avangers");
         System.out.println("Hello Avangers2");
+        System.out.println("Am I a Avangers");
     }
 }
