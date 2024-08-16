f5941eb659e7efdb027c63eafbd443ddbad85c0d
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-08-16 18:44:55 +0200

    fix

--- src/main/java/de/ricardo/Main.java
+++ src/main/java/de/ricardo/Main.java
@@ -3,6 +3,6 @@ package de.ricardo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello Avangers");
+        System.out.println("Hello Avengers");
     }
 }
